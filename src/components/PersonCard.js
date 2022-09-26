b7c77698433eb549add1503d60d8942cd8a044d4
Robert Teets <teetsrobert@gmail.com>
2022-09-26 13:39:05 -0600

    2: finished assignment

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const PersonCard = props => {
     const { firstName, lastName, age, hairColor } = props
+    const [stateAge, setStateAge] = useState(age)
+
+    const handleBirthday = () => {
+        setStateAge(stateAge + 1)
+    }
+
     return (
         <>
             <h2>{ lastName }, { firstName }</h2>
-            <p>Age: { age }</p>
+            <p>Age: { stateAge }</p>
             <p>Hair Color: { hairColor }</p>
+            <button onClick={ handleBirthday }>Birthday Button for {firstName} {lastName}</button>
         </>
     )
 }
